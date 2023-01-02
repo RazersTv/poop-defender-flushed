be23451a8161d9e2563e0e126f3505fd0bd0293a
Razers <101435093+RazersTv@users.noreply.github.com>
2023-01-02 01:07:55 -0500

    local changes

--- poop-defender:flushed/java/flushedpanel.java
+++ poop-defender:flushed/java/flushedpanel.java
@@ -7,40 +7,41 @@ import java.util.*;
 
 //must 'extend' JPanel 
 //You can rename the class to anything you wish - default is 'PanelTemplate'
-public class flushedpanel extends JPanel
-{
-	//variables for the overall width and height
+public class flushedpanel extends JPanel {
+	// variables for the overall width and height
 	private int screenwidth, screenheight;
 	private int crawlerscale, crawlerspeed;
 	private Random rng;
-	
-	//sets up the initial panel for drawing with proper size
-	public flushedpanel(int screenwidth, int screenheight)
-	{
+
+	// sets up the initial panel for drawing with proper size
+	public flushedpanel(int screenwidth, int screenheight) {
 		this.screenwidth = screenwidth;
 		this.screenheight = screenheight;
-		this.setPreferredSize(new Dimension(screenwidth,screenheight));
-		
+		this.setPreferredSize(new Dimension(screenwidth, screenheight));
+
 	}
 
-		
-	//all graphical components go here
-	//this.setBackground(Color c) for example will change background color
-	public void paintComponent(Graphics g)
-	{
-		//this line sets up the graphics - always needed
+	// all graphical components go here
+	// this.setBackground(Color c) for example will change background color
+	public void paintComponent(Graphics g) {
+		// this line sets up the graphics - always needed
 		super.paintComponent(g);
-		//all drawings below here:
+		// all drawings below here:
 
-		//g.drawRect(400, 400, screenwidth, screenheight);
+		// g.drawRect(400, 400, screenwidth, screenheight);
 		g.setColor(Color.green);
-		
+
 		g.fillRect(0, 0, screenwidth, screenheight);
-		g.setColor(Color.green);
+		// g.setColor(Color.green);
 
 		g.setColor(Color.red);
+		g.fillRect(screenwidth - 400, 0, 400, 700);
 
-		g.fillRect(screenwidth/3, screenheight/4, 400, 400);
-		
+		g.setColor(Color.orange);
+		g.fillRect(0, 0, 400, 700);
+
+		g.setColor(Color.black);
+		g.fillRect(0, screenheight / -4, screenwidth + screenwidth, 600);
+
 	}
 }
