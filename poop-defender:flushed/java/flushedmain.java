a40f1fe755b807b5387f2b821c49334ca4bb6262
Razers <101435093+RazersTv@users.noreply.github.com>
2023-01-01 21:06:28 -0500

    merged local commits - prep for first person
    prep for dev showcase

--- poop-defender:flushed/java/flushedmain.java
+++ poop-defender:flushed/java/flushedmain.java
@@ -1,47 +1,24 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 import java.awt.*;
+import java.lang.*;
+import java.io.*;
+import javax.sound.sampled.*;
+import java.util.*;
+import java.awt.event.*;
+/* 
+import org.json.simple.*;
+import org.json.simple.parser.*;
+ */
 
-import java.io.File;
-import java.io.IOException;
-
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-
-public class flushedmain // Main class
-{
-
-	// Lobby Music logic
-	Long currentFrame;
-	Clip clip;
-
-	String status;
-
-	AudioInputStream audioInputStream;
-	static String filePath;
-
-	// intializes streams and audio clips
-	public void AudioPlayer()
-			throws UnsupportedAudioFileException,
-			IOException, LineUnavailableException {
-		// create AudioInputStream object
-		audioInputStream = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
-
-		// create clip reference
-		clip = AudioSystem.getClip();
-
-		// open audioInputStream to the clip
-		clip.open(audioInputStream);
-
-		clip.loop(Clip.LOOP_CONTINUOUSLY);
-	}
-
+public class flushedmain
+{	
 	public static void main(String[] args) {
 
 		// Logic for getting users screen size.
 		Dimension Size = Toolkit.getDefaultToolkit().getScreenSize();
+
+		KeyListener Movement = new KeyListener();
+
 
 		// To be added, fullscreen feature + config
 
@@ -50,6 +27,21 @@ public class flushedmain // Main class
 
 		// sets up a JFrame object with title "Template"
 		JFrame frame = new JFrame("Poop Defender: Flushed");
+		//JButton testclose = new JButton("Close - Dev Environment");
+		//testclose.setBounds(400, 400, 200, 200);
+
+
+		ImageIcon icon = new ImageIcon("C:\\Connolly AP Comp Sci\\Poop-Defender\\Development Repo\\poop-defender-flushed-main\\poop-defender-flushed-main\\poop-defender_flushed\\assets\\lobby-background.png");
+/* 
+		JButton.addActionListener(new ActionListener(){
+			public void actionPerformed('ActionEvent'){
+				System.exit(0);
+			}
+		})
+*/
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+		//frame.setUndecorated(true);
+		frame.setVisible(true);
 		// make sure the jframe closes when you hit the 'x'
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// adds the drawing panel to the frame
@@ -57,19 +49,32 @@ public class flushedmain // Main class
 		// resizes the frame to fit the panel
 		frame.pack();
 		// makes it visible
-		frame.setVisible(true);
+		//frame.setVisible(true);
 
+		//frame.add(testclose);
+		frame.setVisible(true);		
+		//frame.setFocusable(true);
 		// Lobby Music logic
 		Long currentFrame;
 		Clip clip;
 
+		
+
+
+		/*testclose.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				System.exit(0);
+			}	
+		});
+		*/
+		/* 
 		String status;
 		try{
 			filePath = "Please input your filepath for the \"assets\" folder. You will only need to do this one time.";
 			AudioPlayer audioPlayer = new AudioPlayer();
 		}
 		// intializes streams and audio clips
-
+		*/
 	}
 
 }
