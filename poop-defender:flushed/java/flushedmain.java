26715a2995ca854d036ed872ac5f93ace855344d
Razers <101435093+RazersTv@users.noreply.github.com>
2023-01-02 01:07:05 -0500

    local changes

--- poop-defender:flushed/java/flushedmain.java
+++ poop-defender:flushed/java/flushedmain.java
@@ -10,15 +10,13 @@ import org.json.simple.*;
 import org.json.simple.parser.*;
  */
 
-public class flushedmain
-{	
+public class flushedmain {
 	public static void main(String[] args) {
 
 		// Logic for getting users screen size.
 		Dimension Size = Toolkit.getDefaultToolkit().getScreenSize();
 
-		KeyListener Movement = new KeyListener();
-
+		// KeyListener Movement = new KeyListener();
 
 		// To be added, fullscreen feature + config
 
@@ -27,20 +25,20 @@ public class flushedmain
 
 		// sets up a JFrame object with title "Template"
 		JFrame frame = new JFrame("Poop Defender: Flushed");
-		//JButton testclose = new JButton("Close - Dev Environment");
-		//testclose.setBounds(400, 400, 200, 200);
+		// JButton testclose = new JButton("Close - Dev Environment");
+		// testclose.setBounds(400, 400, 200, 200);
 
-
-		ImageIcon icon = new ImageIcon("C:\\Connolly AP Comp Sci\\Poop-Defender\\Development Repo\\poop-defender-flushed-main\\poop-defender-flushed-main\\poop-defender_flushed\\assets\\lobby-background.png");
-/* 
-		JButton.addActionListener(new ActionListener(){
-			public void actionPerformed('ActionEvent'){
-				System.exit(0);
-			}
-		})
-*/
-		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
-		//frame.setUndecorated(true);
+		ImageIcon icon = new ImageIcon(
+				"C:\\Connolly AP Comp Sci\\Poop-Defender\\Development Repo\\poop-defender-flushed-main\\poop-defender-flushed-main\\poop-defender_flushed\\assets\\lobby-background.png");
+		/*
+		 * JButton.addActionListener(new ActionListener(){
+		 * public void actionPerformed('ActionEvent'){
+		 * System.exit(0);
+		 * }
+		 * })
+		 */
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+		// frame.setUndecorated(true);
 		frame.setVisible(true);
 		// make sure the jframe closes when you hit the 'x'
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -48,33 +46,55 @@ public class flushedmain
 		frame.getContentPane().add(new flushedpanel(screenwidth, screenheight));
 		// resizes the frame to fit the panel
 		frame.pack();
-		// makes it visible
-		//frame.setVisible(true);
 
-		//frame.add(testclose);
-		frame.setVisible(true);		
-		//frame.setFocusable(true);
+		frame.addKeyListener(new MovingForward());
+		// makes it visible
+		// frame.setVisible(true);
+
+		// frame.add(testclose);
+		frame.setVisible(true);
+
+		JLabel goal = new JLabel();
+		goal.setText("Goal");
+
+		goal.setBounds(400, 400, 600, 600);
+		goal.setSize(300, 300);
+
+		frame.add(goal);
+		frame.setVisible(true);
+		// frame.setFocusable(true);
 		// Lobby Music logic
 		Long currentFrame;
 		Clip clip;
 
-		
-
-
-		/*testclose.addActionListener(new ActionListener(){
-			public void actionPerformed(ActionEvent e){
-				System.exit(0);
-			}	
-		});
-		*/
-		/* 
-		String status;
-		try{
-			filePath = "Please input your filepath for the \"assets\" folder. You will only need to do this one time.";
-			AudioPlayer audioPlayer = new AudioPlayer();
-		}
-		// intializes streams and audio clips
-		*/
+		/*
+		 * testclose.addActionListener(new ActionListener(){
+		 * public void actionPerformed(ActionEvent e){
+		 * System.exit(0);
+		 * }
+		 * });
+		 */
+		/*
+		 * String status;
+		 * try{
+		 * filePath =
+		 * "Please input your filepath for the \"assets\" folder. You will only need to do this one time."
+		 * ;
+		 * AudioPlayer audioPlayer = new AudioPlayer();
+		 * }
+		 * // intializes streams and audio clips
+		 */
 	}
 
+}
+
+class MovingForward extends KeyAdapter {
+	public void keyPressed(KeyEvent event) {
+		char ch = event.getKeyChar();
+
+		if (ch == 'w') {
+
+		}
+
+	}
 }
