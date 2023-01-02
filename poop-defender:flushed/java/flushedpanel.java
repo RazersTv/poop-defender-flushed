3a460d4097fad74efbac475e96d22d42ba3840db
Razers <101435093+RazersTv@users.noreply.github.com>
2023-01-01 21:06:55 -0500

    merged local changes - support main

--- poop-defender:flushed/java/flushedpanel.java
+++ poop-defender:flushed/java/flushedpanel.java
@@ -1,6 +1,9 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.awt.Color;
+import java.lang.*;
+import java.util.*;
 
 //must 'extend' JPanel 
 //You can rename the class to anything you wish - default is 'PanelTemplate'
@@ -8,6 +11,8 @@ public class flushedpanel extends JPanel
 {
 	//variables for the overall width and height
 	private int screenwidth, screenheight;
+	private int crawlerscale, crawlerspeed;
+	private Random rng;
 	
 	//sets up the initial panel for drawing with proper size
 	public flushedpanel(int screenwidth, int screenheight)
@@ -17,18 +22,25 @@ public class flushedpanel extends JPanel
 		this.setPreferredSize(new Dimension(screenwidth,screenheight));
 		
 	}
-	
-	
+
+		
 	//all graphical components go here
 	//this.setBackground(Color c) for example will change background color
 	public void paintComponent(Graphics g)
 	{
 		//this line sets up the graphics - always needed
 		super.paintComponent(g);
-		
 		//all drawings below here:
+
+		//g.drawRect(400, 400, screenwidth, screenheight);
+		g.setColor(Color.green);
 		
-		
+		g.fillRect(0, 0, screenwidth, screenheight);
+		g.setColor(Color.green);
+
+		g.setColor(Color.red);
+
+		g.fillRect(screenwidth/3, screenheight/4, 400, 400);
 		
 	}
 }
